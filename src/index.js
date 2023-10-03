b8dde0b12e2fc39f8d791ff3097951f4a3477197
KellyBrianMurimi <kellybrianmurimi@gmail.com>
2023-10-03 13:21:19 +0300

    Done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  document.addEventListener('DOMContentLoaded', () => {
+    let form = document.querySelector('form')
+    form.addEventListener('submit', (e) => {
+      e.preventDefault()
+      handleForm(e.target.Description1.value)
+      form.reset()
+    })
+  })
+
+function handleForm(todo){
+  let li = document.createElement('li')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = 'delete'
+  li.textContent =`${todo} `
+  li.appendChild(btn)
+  console.log(li)
+  document.querySelector('#todo_container').appendChild(li)
+}
+
+function handleDelete(e){
+  e.target.parentName.remove()
+}
